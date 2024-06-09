e01100a33008a93b9f68f33562eba4f5f0a3b6ef
Rathan2005 <manjummuniraj@gmail.com>
2024-06-09 21:47:56 +0530

    Add files via upload

---
+++ Rathan10.py
@@ -0,0 +1,9 @@
+basic=float(input())
+print("Basic salary:",basic)
+DA=0.4*basic
+HRA=0.2*basic
+GS=basic+DA+HRA
+print("Rajesh Basic salary is:",GS)
+
+
+
---
+++ Rathan12.py
@@ -0,0 +1,13 @@
+sec=int(input("In seconds: "))
+rad=int(input("\nradius: "))
+hig=int(input("\nheight: "))
+min1=sec/60
+hur=min1/60
+sec=0
+print("Hours: ",hur)
+print("Minutes: ",min1)
+print("Seconds: ",sec)
+vol=3.14*rad**2*hig
+print("volume: %1f"%vol1)
+
+
